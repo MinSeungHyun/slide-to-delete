41969ce42ce316ea78fd7b841e9a1687351bb833
MinSeungHyun <asdf014563@naver.com>
2019-08-15 20:20:20 +0900

    뷰 이동 성공

--- app/src/main/java/com/seunghyun/dragtodelete/MainActivity.kt
+++ app/src/main/java/com/seunghyun/dragtodelete/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.seunghyun.dragtodelete
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var firstX = 0f
+    private var firstViewX = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        deleteTV.setOnTouchListener { _, event ->
+            val x = event.x
+
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    firstX = x
+                    firstViewX = text.x
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val distance = x - firstX
+                    text.x = firstViewX + distance
+                }
+            }
+            return@setOnTouchListener true
+        }
     }
 }
